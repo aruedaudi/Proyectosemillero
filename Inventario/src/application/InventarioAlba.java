5d76da8e09d3f9408b8af59896cac40792f8be80
FelipeTapiasM <160045197+FelipeTapiasM@users.noreply.github.com>
2025-10-09 19:10:05 -0500

    Prueba de proyecto

---
+++ Inventario/src/application/InventarioAlba.java
@@ -0,0 +1,16 @@
+package application;
+
+public class InventarioAlba {
+
+	private String cantidadTotal;
+	public String getCantidadTotal() {
+		return cantidadTotal;
+	}
+	public void setCantidadTotal(String cantidadTotal) {
+		this.cantidadTotal = cantidadTotal;
+	}
+	public InventarioAlba(String cantidadTotal) {
+		this.cantidadTotal=cantidadTotal;
+	}
+
+}
