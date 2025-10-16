7f33ecfb6e94f4b2bc678e078eb582f92da96925
KevinSantiagoSS <kserrano7@udi.edu.co>
2025-10-15 21:52:53 -0500

    Conflicto resuelto en DatosInventario.java

--- Inventario/src/application/DatosInventario.java
+++ Inventario/src/application/DatosInventario.java
@@ -7,25 +7,25 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 
-/*/
+/**
  * BASE DE DATOS PARA LA CANTIDAD TOTAL DEL INVENTARIO
  */
 public class DatosInventario {
-	 //Metodo que obtiene todos los registros del inventario
+    // Método que obtiene todos los registros del inventario
     public LinkedList<InventarioAlba> getDatos() {
         LinkedList<InventarioAlba> data = new LinkedList<>();
 
         try {
-            // Conexion a Oracle
+            // Conexión a Oracle
             Connection conn = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@localhost:1521:xe", 
-                    "ALBA_CACAO",
-                    "ALBA_CACAO"
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "ALBA_CACAO",
+                "ALBA_CACAO"
             );
 
             Statement st = conn.createStatement();
 
-            // 
+            // Consulta a la vista
             String query = "SELECT * FROM vista_inventario";
 
             ResultSet result = st.executeQuery(query);
@@ -33,23 +33,13 @@ public class DatosInventario {
             while (result.next()) {
                 InventarioAlba item = new InventarioAlba(
                     result.getInt("ID"),
-<<<<<<< HEAD
-                    result.getString("Producto"),
-                    result.getInt("Cantidad"),
-                    result.getFloat("Peso"),
-                    result.getString("Estado"),
-                    result.getFloat("Humedad"),
-                    result.getString("Ultimo_Mov"),
-                    result.getString("Proveedor")
-=======
                     result.getString("PRODUCTO"),
                     result.getInt("CANTIDAD"),
                     result.getFloat("PESO"),
                     result.getString("ESTADO"),
                     result.getFloat("HUMEDAD"),
-                    result.getString("FECHA_ING"),
+                    result.getString("FECHA_MOV"),
                     result.getString("PROVEEDOR")
->>>>>>> f5fa161f1ec18ddf89caf6e9a303e49e4d08983b
                 );
                 data.add(item);
             }
@@ -63,4 +53,3 @@ public class DatosInventario {
         return data;
     }
 }
-
